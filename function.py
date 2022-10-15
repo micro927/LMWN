8d6c1feb649d7a76eb44359f21eb0703b0f58624
micro927 <nsbelieve-15@hotmail.com>
2022-10-15 15:13:14 +0700

    donw: i got result!

--- function.py
+++ function.py
@@ -1,84 +1,29 @@
-def printZigZagConcat(str, n):
-
-    # Corner Case (Only one row)
-    if n == 1:
-        print(str)
-        return
-
-    # Find length of string
-    l = len(str)
-
-    # Create an array of
-    # strings for all n rows
-    arr = ["" for x in range(l)]
-
-    # Initialize index for
-    # array of strings arr[]
-    row = 0
-
-    # Traverse through
-    # given string
-    for i in range(l):
-
-        # append current character
-        # to current row
-        arr[row] += str[i]
-
-        # If last row is reached,
-        # change direction to 'up'
-        if row == n - 1:
-            down = False
-
-        # If 1st row is reached,
-        # change direction to 'down'
-        elif row == 0:
-            down = True
-
-        # If direction is down,
-        # increment, else decrement
-        if down:
-            row += 1
-        else:
-            row -= 1
-
-    # Print concatenation
-    # of all rows
-    for i in range(n):
-        print(arr[i], end="")
+import collections
 
 
-def zigzag(arr):
-    n = len(arr)
-    # Flag true indicates relation "<" is expected,
-    # else ">" is expected. The first expected relation
-    # is "<"
-    flag = True
-    for i in range(n-1):
-        # "<" relation expected
-        if flag is True:
-            # If we have a situation like A > B > C,
-            # we get A > C < B
-            # by swapping B and C
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-            # ">" relation expected
-        else:
-            # If we have a situation like A < B < C,
-            # we get A < C > B
-            # by swapping B and C
-            if arr[i] < arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-        flag = bool(1 - flag)
-    return (arr)
+def sort_string(string):
+    string = ''.join(sorted(string))
+    return string
 
 
-def zigzag_sort(arr):
-    n = len(arr)
-    # use sort function to sort the array
-    arr.sort()
-    # traverse the array from 1 to n-1
-    for i in range(1, n-1, 2):
-      # swap value of current element with next element
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-    # print the array
-    return (arr)
+def string_to_array(string):
+    return [char for char in string]
+
+
+def array_to_string(array):
+    return ''.join(array)
+
+
+def filter_chars_out_from_string(string, filterout_array):
+    for char in filterout_array:
+        string = string.replace(char, "")
+    return string
+
+
+def count_char_in_string(string):
+    frequencies = collections.Counter(string)
+    count_dict = {}
+    for key, value in frequencies.items():
+        if value > 0:
+            count_dict[key] = value
+    return dict(sorted(count_dict.items(), key=lambda item: item[1]))
--- solve.js
+++
@@ -1,2 +0,0 @@
-var whatIsIt = "CYtZBsWZaZliYZocWLZlXuZZYWYeYXZsXeZXtXWpXeRYYYd!ZnYeWXoYXasnX,WXWrWPoAdWesnciGenWr"
-console.log(whatIsIt.length)
